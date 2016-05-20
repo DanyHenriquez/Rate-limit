905c480be63a3709bb2c5342074e93d0f7b535ce
Dany Henriquez <Prezto@users.noreply.github.com>
2016-05-20 12:12:06 +0200

    Update RateLimitMiddleware.php

--- src/RateLimitMiddleware.php
+++ src/RateLimitMiddleware.php
@@ -13,15 +13,41 @@ use Flintstone\Flintstone;
 
 class RateLimitMiddleware
 {
+    /**
+     * @var null|string
+     */
+    public $host = null;
+
+    /**
+     * @var null|string
+     */
+    public $port = null;
+
+    /**
+     * @var null
+     */
+    public $pass = null;
+
     protected $handle = null;
 
-    protected $maxRequests = 60;
+    protected $maxRequests = 1;
 
     protected $seconds = 10;
 
-    public function __construct($maxRequests = 60, $seconds = 10)
+    public function __construct($host = 'localhost', $port = '6379', $pass = null)
     {
-        $this->handle = new Flintstone($_SERVER["REMOTE_ADDR"], array('dir' => sys_get_temp_dir() . '/'));
+        $this->host = $host;
+        $this->port = $port;
+        $this->pass = $pass;
+
+        $this->handle = new \TinyRedisClient(sprintf("%s:%s", $this->host, $this->port));
+
+        if ($this->pass !== null)
+            $this->auth();
+    }
+
+    public function setRequestsPerSecond($maxRequests, $seconds)
+    {
         if (!is_int($maxRequests))
             throw new \InvalidArgumentException;
 
@@ -32,26 +58,23 @@ class RateLimitMiddleware
         $this->seconds = $seconds;
     }
 
-    private function increment()
+    /**
+     *
+     */
+    public function auth()
     {
-        $this->handle->set(microtime(), time() + $this->seconds);
-    }
-
-    private function purgeExpired()
-    {
-        foreach ($this->handle->getKeys() as $key) {
-            if ($this->handle->get($key) > time())
-                $this->handle->delete($key);
-        }
+        $this->handle->auth($this->pass);
     }
 
     public function __invoke($request, $response, $next)
     {
-        $this->purgeExpired();
-        $this->increment();
-
-        if (count($this->handle->getKeys()) > $this->maxRequests)
+        if (count($this->handle->keys(sprintf("%s*", str_replace('.', '', $_SERVER['REMOTE_ADDR'])))) >= $this->maxRequests)
             $response = $response->withStatus(429);
+        else {
+            $key = sprintf("%s%s", str_replace('.', '', $_SERVER['REMOTE_ADDR']), mt_rand());
+            $this->handle->set($key, time());
+            $this->handle->expire($key, $this->seconds);
+        }
 
         $response = $next($request, $response);
 
